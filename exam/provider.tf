fad9cf2f09c1c5661d008bd6ee5b3d84a9eaa4d0
An0802 <157581272+An0802@users.noreply.github.com>
2024-02-26 16:12:37 +0530

    Add files via upload

---
+++ exam/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.0"
+}
+}
+}
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = "ASIASZ3EE3HN2FDF5G5Z"
+    secret_key = "VRs8QG0hRuanaPs8pP9WLJi7Xy5dvRqkrfNGGUIz"
+    token = "FwoGZXIvYXdzEOT//////////wEaDAWG8wErF7CBlz7fgSKvAVbi9Twotf0GK82vjg69VqE/po62SSE33/Jx22Ry2MplgKy70l7Tdg+Kh8kFs2zqwjD3EzIhxtepyc8imU93THl0x4njmEaSs8KjtLotM7GlHUmypx1aILlGA3R1S5fc6aTlPcUpNA9OebnJlnQDI+YIpM7w/A1NSL8uBX4oG64E8JM7UMsdKyhDZ0lqua2IgXBvTR3ftfbEImuam2o4/ZhYtA9kpoevIFQEtp4VWC0o58TxrgYyLfJO3BVUG+EGEKfqhbFdkU4aWetpjdlT8sFYGP8905xZvcNe6HO+4JQkgDMEhw=="
+
+}
